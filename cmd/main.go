5fb9b029d59013172c2c34efbea3a99c0dcff364
Julian Bertini <julianbertini@gmail.com>
2019-10-01 00:59:46 -0500

    restructure project into proper libraries

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,34 @@
-// will eventually be simple main function that calls everything else
+package main
+
+import (
+	"log"
+
+	"github.com/julianbertini/autoList/internal/recipe"
+	"github.com/julianbertini/autoList/internal/sheetswrapper"
+)
+
+func main() {
+
+	spreadsheetID := "1sLGhnXgDLig0HTJE4_uQ6OXXc4iQO4U5QdqWGfyyBck"
+	readRange := "Recipes!A:Z"
+	service := sheetswrapper.GetService()
+	resp, err := service.Spreadsheets.Values.Get(spreadsheetID, readRange).Do()
+
+	if err != nil {
+		log.Fatalf("Unable to retrieve data from sheet: %v", err)
+	}
+
+	// need to identify indices of headers
+	headersMap := recipe.GetHeaders(resp.Values)
+	// fmt.Println(headersMap)
+
+	groceryMap := make(map[string][]string)
+
+	ingredients := recipe.GetIngredients(resp.Values, "1-M", headersMap)
+	units := recipe.LoadUnitConversions()
+
+	recipe.AddIngredientsToList(ingredients, units, groceryMap)
+
+	recipe.SaveListToFile("groceryList.txt", groceryMap)
+
+}
